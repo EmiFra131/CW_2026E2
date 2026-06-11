001da648d0b33e968db1b8bdc36af376f879eb19
Emilio <324292763@alumno.enp.unam.mx>
2026-06-10 21:53:23 -0600

    feature(index.php): implementacion de cookies de inicio de sesion

--- index.php
+++ index.php
@@ -1,63 +1,66 @@
 <?php
 
+session_start();
+
 include './include/db.php';
 include './include/validacion.php';
 
-session_start();
+
 
 if (isset($_POST["correo"])){
 
     $con = connect();
 
-    $usuario = trim($_POST["correo"]); // Quitamos espacios al inicio y al final del string recibido por formulario
-    $password = trim($_POST["password"]);
+    $correo = trim($_POST["correo"]); // Quitamos espacios al inicio y al final del string recibido por formulario
+    $contraseña = trim($_POST["password"]);
 
-    $query = "SELECT c.id_cuenta, c.correo, c.nombre, c.contraseña t.rol FROM cuenta c 
+    $query = "SELECT c.id_cuenta, c.correo, c.nombre, c.contraseña, t.rol FROM cuenta c 
     INNER JOIN tipo_usuario t ON c.id_tipo_usuario = t.id_tipo_usuario
-    WHERE c.correo = '$usuario'"; 
+    WHERE c.correo = '$correo'"; 
     
     $result = mysqli_query( $con, $query);
     $registro = mysqli_fetch_assoc($result);
     //echo $query;
     //var_dump($registro);
 
-    /*             
+           
 
     if ($registro){
         $hash_base_de_datos = $registro["contraseña"];
-        password_verify($password, $hash_base_de_datos) // Verificamos que coincidan usu y pass
-        $_SESSION['usuario'] = $registro["nocta"];
-        $_SESSION["rol"] = "usuario";
-        $_SESSION["nombre_completo"] = $registro["nombre"] . " " . $registro["appat"] . " " . $registro["apmat"];
-        setcookie("usuario", $registro["nocta"], time() + (86400)); // 1 dia = 86400 segundos, expirará en un dia
+        if(password_verify($contraseña, $hash_base_de_datos)){
+            $_SESSION['usuario'] = $registro["correo"];
+            $_SESSION["rol"] = $registro["rol"];
+            $_SESSION["nombre_completo"] = $registro["nombre"];
+            setcookie("usuario", $registro["correo"], time() + (86400)); // 1 dia = 86400 segundos, expirará en un dia
         header("Location: usuario.php");
+        exit();
+        }
     } else {
         $error = "No coinciden usuario o contraseña";
     }
-    */
-/*
+
 } else {
     // Verificamos que tenga la cookie
     if (isset($_COOKIE["usuario"])){
+        $con = connect();
         $usuario = $_COOKIE["usuario"];
 
-        $query = "SELECT nocta, nombre, appat, apmat, password FROM usuarios WHERE nocta = $usuario";
+        $query = "SELECT c.id_cuenta, c.correo, c.nombre, c.contraseña, t.rol FROM cuenta c 
+        INNER JOIN tipo_usuario t ON c.id_tipo_usuario = t.id_tipo_usuario
+        WHERE c.correo = '$usuario'"; 
         $result = mysqli_query( $con, $query);
         $registro = mysqli_fetch_assoc($result);
 
-        $_SESSION['usuario'] = $registro["nocta"];
-        $_SESSION["rol"] = "usuario";
-        $_SESSION["nombre_completo"] = $registro["nombre"] . " " . $registro["appat"] . " " . $registro["apmat"];
-        setcookie("usuario", $registro["nocta"], time() + (86400)); // 1 dia = 86400 segundos, expirará en un dia
-        header("Location: usuario.php");
+        $_SESSION['usuario'] = $registro["correo"];
+        $_SESSION["rol"] = $registro["rol"];
+        $_SESSION["nombre_completo"] = $registro["nombre"];
+        setcookie("usuario", $registro["correo"], time() + (86400)); // 1 dia = 86400 segundos, expirará en un dia
+        header("Location: templates/alumno_tareas.html");
 
     }
 }
-*/
-}
+
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -78,7 +81,7 @@ if (isset($_POST["correo"])){
         <img src="./statics/img/usuario.png" alt="Icono de perfil de usuario" id="icono_perfil">
         <form id="formulario_inicio" method="post" action="index.php">
             <label>USUARIO:</label>
-            <input type="text" placeholder="correo" minlength="9" maxlength="9" name = "correo" required>
+            <input type="text" placeholder="correo" name = "correo" required>
             <span></span> <!--si el usuario ingresa mal los datos aquí le aparecerá el aviso-->
             <label>CONTRASEÑA:</label>
             <input type="password" placeholder="contrasena" name = "password" required>
